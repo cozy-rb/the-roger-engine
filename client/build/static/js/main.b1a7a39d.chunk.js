947803ed80b9ef86314d0b595ff70f0b7b2f1b76
unknown <incaseofcozy@gmail.com>
2021-02-26 13:04:12 -0600

    🎈 Initial Commit

---
+++ client/build/static/js/main.b1a7a39d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n.n(c),a=n(2),s=n.n(a);n(8);var i=function(){return r.a.createElement("div",{className:"App"},"React Express Boilerplate")};s.a.render(r.a.createElement(i,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.b1a7a39d.chunk.js.map
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      React Express Boilerplate
+    </div>
+  );
+}
+
+export default App;
